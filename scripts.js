1e1f335000172de51f4627ea942a5106898370e6
Zac <zacace@ingoglia.com>
2020-02-13 15:28:05 -0700

    EOD 02/13/2020 - Zac
    This descriptions is for this commit and the previous one:
    * Created JS function to change CSS styles that are loaded
    * Added multiple stylesheets that can be selected by the JS function
    * Commented JS Code

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
-function changeCSS(cssFile) {
+function replaceCSS(newStyles) {
 
     //grabs the css page to replace with the user's selection
     var startingStyles = document.getElementsByTagName("link")[0];
 
+    //Creates the element that will be injected to the page
     var replacementStyle = document.createElement("link");
+    //Sets the rel attribute to stylesheet in the injected element
     replacementStyle.setAttribute("rel", "stylesheet");
-    replacementStyle.setAttribute("href", cssFile);
+    //adds the href attribute to the created element with the CSS page specified in the button
+    replacementStyle.setAttribute("href", newStyles);
 
+    //this is where the new stylesheet replaces the old/original one
     document.getElementsByTagName("head")[0].replaceChild(replacementStyle, startingStyles);
 }
